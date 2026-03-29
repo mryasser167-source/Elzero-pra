79dfb620c2a034d8005140faad5258f73172c5f6
mryasser167-source <mryasser167@gmail.com>
2026-03-29 10:41:28 -0700

    Delete BOM-prompt.js

--- BOM-prompt.js
+++
@@ -1,9 +0,0 @@
-let prompt1 = prompt("Print Number From – To", "Example: 5-20");
-let array = prompt1.split("");
-let start = Number(array[0]);
-let end = Number(array[2] + array[3]);
-let min = Math.min(start, end);
-let max = Math.max(start, end);
-for (let i = min; i <= max; i++) {
-  console.log(i);
-}
