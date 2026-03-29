1ab1c407851d3a0a297f540a242c78eed63b3173
mryasser167-source <mryasser167@gmail.com>
2026-03-29 10:42:08 -0700

    Delete Window History.js

--- Window History.js
+++
@@ -1,5 +0,0 @@
-console.log(history.length); // history/عدد الصفاdحات اال موجوده في histor
-history.back(); //بيرجع للصفحه اللي ورا
-history.forward(); //بيطلع لقدام صفحه\
-history.go(رقم); //بنستخدمه في رجوع او طلوع //لو رجوع يبق بالسالب //لو طلوع يبق بالموجب
-///
