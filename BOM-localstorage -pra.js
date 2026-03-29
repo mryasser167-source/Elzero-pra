0996cea0d3d0cbc88526bc98d0ed128818fc70b4
mryasser167-source <mryasser167@gmail.com>
2026-03-29 10:40:56 -0700

    Delete BOM-localstorage -pra.js

--- BOM-localstorage -pra.js
+++
@@ -1,9 +0,0 @@
-let input = document.querySelector("input");
-input.onblur = function () {
-  window.localStorage.setItem("name", input.value);
-};
-window.addEventListener("load", function () {
-  if (window.localStorage.getItem("name")) {
-    input.value = window.localStorage.getItem("name");
-  }
-});
